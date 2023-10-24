b7ba0492a35ea390332c2c06079b84609a282855
Tha-Orakkle <adegbiranayinoluwa.paul@yahoo.com>
2023-10-24 09:49:25 +0100

    storage engines: file storage and Database. These files will be updated as new models are created

---
+++ models/engine/file_storage.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""File Storage Engine"""
+import json
+from models.base_model import Basemodel
+
+classes = {"Basemodel": Basemodel}
+
+
+class FileStorage:
+    """creates file storage by serializing and deserializing instancd"""
+    
+    # dictionary to store all objects by the class and id
+    __objects = {}
+    
+    # JSON file location
+    __file_path = "eLearnHub.json"
+    
+    def all(self, cls=None):
+        """returns all objects or the objects of a specific class"""
+        if cls is not None:
+            new_obj_list = {}
+            for k, v in self.__objects.items():
+                if cls == v.__class__ or cls == v.__class__.__name__:
+                    new_obj_list[k] = v
+            return new_obj_list
+        return self.__objects
+    
+    def new(self, obj):
+        """adds new object to __objects with the key <class>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            self.__objects[key] = obj
+        
+    def save(self):
+        """serializes __object to a json file"""
+        json_dict = {}
+        for k, v in self.__objects.items():
+            json_dict[k] = v.to_dict()
+        with open(self.__file_path, "w") as f:
+            json.dump(json_dict, f)
+        
+    def delete(self, obj):
+        """deletes a item from the __object"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+                
+    def get(self, cls, id):
+        """gets an object based on class name and id"""
+        if cls not in classes.values():
+            return None
+        from models import storage
+        all_clss = storage.all(cls)
+        for v in all_clss.values():
+            if (v.id == id):
+                return v
+        return None
+    
+    def reload(self):
+        """deserializes JSOn file to __objects"""
+        try:
+            with open(self.__file_path, "r") as f:
+                data = json.load(f)
+                for k, v in data.items():
+                    self.__objects[k] = classes[v["__class__"]](**v)
+        except:
+            pass
+        
+    def close(self):
+        """calls the reload method"""
+        self.reload()
